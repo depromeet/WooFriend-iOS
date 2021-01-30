6837a347988c7c84d5137cdcbce59ac7ca9c02a0
qbbang <qbbang@gmail.com>
2021-01-30 12:33:06 +0900

    env: iOS12 타켓 프로젝트 기본 설정

---
+++ Woofriend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  woofriend
+//
+//  Created by 이규현 on 2021/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WoofriendTests/woofriendTests.swift
@@ -0,0 +1,33 @@
+//
+//  woofriendTests.swift
+//  woofriendTests
+//
+//  Created by 이규현 on 2021/01/25.
+//
+
+import XCTest
+@testable import woofriend
+
+class woofriendTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
